ae4e0569a097d64da882cd39261d55d6fdc5391b
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-05-18 17:28:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,25 @@ function setup()
     video.size=(500,400);
     canvas=createCanvas(600,445); 
     canvas.position(800,143);
+
+    poseNet=ml5.poseNet(video,modelLoaded); 
+    poseNet.on('pose',gotPoses);
+}
+
+function draw()
+{
+background('#FF00FF');
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if (results.length>0)
+    {
+        console.log(results);
+    }
 }
